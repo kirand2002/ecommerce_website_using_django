de62c88c92b7e9d4644eeeecf99c7dda0584d304
kirand2002 <kirandhanushraju@gmail.com>
2025-02-25 23:37:49 +0530

    updated

--- payment/views.py
+++ payment/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
 from cart.cart import Cart
 
 
-from payment.forms import ShippingForm
-from payment.models import ShippingAddress
+
+from payment.forms import ShippingForm,PaymentForm
+from payment.models import ShippingAddress,Order,OrderItem
+from django.contrib.auth.models import User
+from store.models import Product
 
 
 def payment_success(request):
@@ -26,6 +30,8 @@ def checkout(request):
          #get user shipping form
         shipping_form = ShippingForm(request.POST or None, instance=shipping_user)
 
+        
+
    
         return render(request, "payment/checkout.html", {'cart_products': cart_products,"quantities":quantities,'totals':totals,"shipping_form":shipping_form})
     
@@ -35,5 +41,145 @@ def checkout(request):
 
         return render(request, "payment/checkout.html", {'cart_products': cart_products,"quantities":quantities,'totals':totals,"shipping_form":shipping_form})
 
+def billing_info(request):
+    if request.POST:
+        #get the cart
+        cart = Cart(request)
+
+        cart_products = cart.get_prods
+        quantities = cart.get_quants
+        totals= cart.cart_total()
+
+        #create the session with shipping info
+
+        my_shipping = request.POST
+        request.session['my_shipping'] = my_shipping
 
 
+
+       #check if user is logged in or not
+        if request.user.is_authenticated:
+            #for logged users
+            #get billing Form
+            billing_form = PaymentForm()
+
+            return render(request, "payment/billing_info.html", {'cart_products': cart_products,"quantities":quantities,'totals':totals,"shipping_info":request.POST,'billing_form':billing_form})
+        else:
+            # for not logged users
+            #get billing Form
+            billing_form = PaymentForm()
+            return render(request, "payment/billing_info.html", {'cart_products': cart_products,"quantities":quantities,'totals':totals,"shipping_info":request.POST,'billing_form':billing_form})
+    
+    else:
+        messages.success(request,"Access Denied")
+        return redirect('home')
+    
+
+def process_order(request):
+    if request.POST:
+
+        #get the cart
+        cart = Cart(request)
+
+        cart_products = cart.get_prods
+        
+        quantities = cart.get_quants
+        
+        totals= cart.cart_total()
+
+        # getting billing info form the last page
+        payment_form = PaymentForm(request.POST or None)
+
+        
+
+        # get shipping session Data
+        my_shipping = request.session.get('my_shipping')
+
+        #gather the info
+        full_name = my_shipping['shipping_full_name']
+
+        email = my_shipping['shipping_email']
+        
+        
+        #create shipping address from session info
+
+        shipping_address =f"{my_shipping['shipping_address1']}\n{my_shipping['shipping_address2']}\n{my_shipping['shipping_city']}\n{my_shipping['shipping_state']}\n{my_shipping['shipping_zipcode']}\n{my_shipping['shipping_country']}"
+        
+        amount_paid = totals
+
+        #create an order
+
+        if request.user.is_authenticated:
+            #logged in
+            user = request.user
+            #create order
+            create_order = Order(user=user, full_name=full_name, email=email, shipping_address=shipping_address, amount_paid=amount_paid)
+            create_order.save()
+
+            #GET THE ORDER ID
+            order_id = create_order.pk
+
+            #get the product info
+            for product in cart_products():
+                #get product id
+                product_id = product.id
+
+                #get product price
+                if product.is_onsale:
+                    price = product.sale_price
+                else:
+                    price =product.price
+
+                #get Quantity
+
+                for key,value in quantities().items():
+                    if int(key) == product.id:
+                        #create order item
+                        create_order_item = OrderItem(order_id=order_id, user=user, product_id=product_id, quantity=value, price=price)
+                        create_order_item.save()
+
+            
+            
+            
+            messages.success(request,'Order Placed')
+            return redirect('home')
+        else:
+            #not logged in
+            create_order = Order(full_name=full_name, email=email, shipping_address=shipping_address, amount_paid=amount_paid)
+            create_order.save()
+
+            #GET THE ORDER ID
+            order_id = create_order.pk
+
+            #get the product info
+            for product in cart_products():
+                #get product id
+                product_id = product.id
+
+                #get product price
+                if product.is_onsale:
+                    price = product.sale_price
+                else:
+                    price =product.price
+
+                #get Quantity
+
+                for key,value in quantities().items():
+                    if int(key) == product.id:
+                        #create order item
+                        create_order_item = OrderItem(order_id=order_id, product_id=product_id, quantity=value, price=price)
+                        create_order_item.save()
+
+            messages.success(request,'Order Placed')
+            return redirect('home')
+        
+
+    else:
+        messages.success(request,"Access Denied")
+        return redirect('home')
+    
+
+
+
+
+    
