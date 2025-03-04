c86dc7fa7d44efaa7d6d8c1d400ba690d154764e
kirand2002 <kirandhanushraju@gmail.com>
2025-03-04 20:32:21 +0530

    added

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -13,8 +13,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
-import pymysql
-pymysql.install_as_MySQLdb()
+# import pymysql
+# pymysql.install_as_MySQLdb()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -100,14 +100,14 @@ WSGI_APPLICATION = "ecommerce.wsgi.application"
 
 DATABASES = {
     "default": {
-        # "ENGINE": "django.db.backends.sqlite3",
-        # "NAME": BASE_DIR / "db.sqlite3",
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "defaultdb" ,
-        "USER" : "avnadmin" ,
-        "PASSWORD" : os.environ.get('DB_PASSWORD_ME') ,
-        "HOST" : "mysql-3582a32c-kirandhanushraju-9a5c.i.aivencloud.com" ,
-        "PORT" : "26224" ,
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+        # "ENGINE": "django.db.backends.mysql",
+        # "NAME": "defaultdb" ,
+        # "USER" : "avnadmin" ,
+        # "PASSWORD" : os.environ.get('DB_PASSWORD_ME') ,
+        # "HOST" : "mysql-3582a32c-kirandhanushraju-9a5c.i.aivencloud.com" ,
+        # "PORT" : "26224" ,
     }
 }
 
